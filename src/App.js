29aeb81cafe5b004a77a52dc483fc429910bc3bf
Wayne Yip <wqyip97@gmail.com>
2024-07-26 17:23:15 +0800

    Install and set up Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './components/About.js';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Navbar />
       <Work />
       <About />
