002c79367e9438be7c781cbbefed5feb3af93ff7
Wayne Yip <wqyip97@gmail.com>
2024-08-01 13:23:15 +0800

    Make Detail component's titles stand out

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,7 +4,7 @@ export default function Detail({ imgSrc, imgAlt, title, text })
     <div className="flex mt-10">
       <img className="w-2/3 pr-10" src={imgSrc} alt={title}/>
       <div className="flex-col text-left">
-        <h2>{title}</h2>
+        <h2 className="mb-2 text-lg font-medium">{title}</h2>
         <p>{text}</p>
       </div> 
     </div>
