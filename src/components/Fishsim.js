a58e5aae69d785128199bd888ad1b1ca48d46247
Wayne Yip <wqyip97@gmail.com>
2024-08-01 13:46:42 +0800

    Add fish sim GitHub link, update text

--- src/components/Fishsim.js
+++ src/components/Fishsim.js
@@ -10,18 +10,18 @@ const details = [
   },
   {
     imgSrc: "work_fish.png", 
-    title: "Fish Shader",
-    text: "I customised the default three.js lambert for the fish. It animates with a sine wave on the vertex shader, and samples a noise texture in world space for the caustics lighting effect."            
+    title: "Fish Shader with Caustics",
+    text: "I customised the default three.js lambert for the fish. It performs a sine wave animation on the vertex shader, and samples a noise texture in world space for the caustics lighting effect."            
   },
   {
     imgSrc: "work_fish.png", 
-    title: "Water Surface Shader",
-    text: "With only one normal map and some math tricks, I got a reflective caustics effect on the water surface shader that picks up light from above."    
+    title: "Water Surface Shader with Specular Caustics",
+    text: "Using two samples of a normal map and some math tricks, I landed on a water surface that faces downwards but picks up specular highlights from the directional light above."    
   },
   {
     imgSrc: "work_fish.png", 
-    title: "Fast Procedural Godrays",
-    text: "God rays are faked with a single cone mesh that uses the same noise texture, sampled with polar coordinates that shift over time."    
+    title: "Fast Procedural God Ray Shader",
+    text: "God rays are faked with a single cone mesh, sampling a noise texture in polar coordinates that shift over time."    
   },
 ]
 
@@ -32,21 +32,28 @@ export default function Fishsim()
       <Navbar />
         <div className="flex-grow">
           <div className="container max-w-screen-lg mx-auto py-20">
-            <h1 className="mb-4 text-left text-3xl font-medium text-gray-800">Three.js Fish Simulation</h1>
+            <h1 className="mb-8 text-left text-3xl font-medium text-gray-800">Three.js Fish Simulation</h1>
             <div className="flex">
               <img className="w-2/3 pr-10" src="work_fish.png" alt="Three.js Fish Simulation"/>
               <div className="flex-col text-left">
                 <p>Role: Solo Developer</p>
                 <p>Tech: Three.js, GLSL, Node.js, Vite, Vercel</p>
                 <br/>
-                <p>An interactive fish simulation that runs on the browser. I built this to consolidate my learnings from Three.js Journey, the comprehensive 93-hour course by Bruno Simon.</p>
+                <p>An interactive fish simulation that runs on the browser. I built this to apply my initial learnings from Three.js Journey, the comprehensive 93-hour course by Bruno Simon.</p>
                 <br/>
                 <a
                   href="https://wayne-ocean.vercel.app/"
                   target="_blank" 
                   rel="noopener noreferrer"
-                  className="py-2 px-6 text-white text-lg bg-green-500 hover:bg-green-600">
+                  className="mr-5 py-2 px-6 text-white text-lg bg-green-500 hover:bg-green-600">
                   Live Demo
+                </a>
+                <a
+                  href="https://github.com/wayneyip/fishtank"
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="mr-5 py-2 px-6 text-white text-lg bg-green-500 hover:bg-green-600">
+                  GitHub
                 </a>
               </div>
             </div>
