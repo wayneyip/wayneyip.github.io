36ff58b934b5e95c79da9afa25d1385717c8f18a
Wayne Yip <wqyip97@gmail.com>
2024-08-01 22:53:49 +0800

    Fix nested linking in Work entries

--- src/components/Work.js
+++ src/components/Work.js
@@ -24,10 +24,7 @@ export default function Home()
               <div key={index} className="relative overflow-hidden bg-cover bg-no-repeat">
                 <Link to={project.siteUrl}>
                   <img src={project.imageUrl} alt={project.title} className="w-full h-auto" />
-                  <a href="#!">
-                    <div
-                      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                  </a>
+                  <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                   <div className="p-2 text-center">
                     <h3 className="text-lg">{project.title}</h3>
                   </div>
