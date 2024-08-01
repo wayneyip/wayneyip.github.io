11333b0eee422f53ad81e0f67218f384c2d01a68
Wayne Yip <wqyip97@gmail.com>
2024-08-01 14:37:43 +0800

    Add hover effect to Work images

--- src/components/Work.js
+++ src/components/Work.js
@@ -21,9 +21,13 @@ export default function Home()
         <section className="text-left mt-24 mb-20">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {projects.map((project, index) => (
-              <div key={index} className="overflow-hidden">
+              <div key={index} className="relative overflow-hidden bg-cover bg-no-repeat">
                 <Link to={project.siteUrl}>
                   <img src={project.imageUrl} alt={project.title} className="w-full h-auto" />
+                  <a href="#!">
+                    <div
+                      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                  </a>
                   <div className="p-2 text-center">
                     <h3 className="text-lg">{project.title}</h3>
                   </div>
