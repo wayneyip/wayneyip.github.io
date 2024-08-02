5ee5dff561494fa27e75544ef542c9b18ea35b23
Wayne Yip <wqyip97@gmail.com>
2024-08-02 15:37:11 +0800

    Tweak styling on Back button

--- src/components/BackToHome.js
+++ src/components/BackToHome.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function BackToHome()
 {
 	return (
-		<div className="container max-w-screen-lg py-8">
+		<div className="container max-w-screen-lg pb-10">
 		<Link to="/">
-			<p className="text-left">&lt; Back To Home</p>
+			<p className="text-blue-600 hover:text-blue-400">&lt; Back to Work</p>
 		</Link>
 		</div>
 	)
