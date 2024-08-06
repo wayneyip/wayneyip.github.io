2be53f6f9f1014c92f74d9c46d279f8006e2865c
Wayne Yip <wqyip97@gmail.com>
2024-08-06 16:06:45 +0800

    Finish up implementation of Pixelart images and videos

--- src/components/Pixelart.js
+++ src/components/Pixelart.js
@@ -10,53 +10,40 @@ const outlineDetails = [
     text: "By painting vertex colors, the artist gets to define where outlines are drawn, which gives more stable and controllable results than the traditional depth- or normals-based outlines."    
   },
   {
-    imgSrc: "work_pixelart.png", 
+    imgSrc: "pixel_outlinepass.png", 
+    title: "1-Pixel Outlines",
+    text: "Outlines are drawn by sampling the four orthogonal neighbours of each pixel, never diagonal, resulting in the characteristic 1-pixel-wide outlines found in pixel art."    
+  },
+  {
+    imgSrc: "pixel_lightpass.png", 
+    title: "Colored by Lights",
+    text: "Outlines are realtime-lit in the main light's direction. To avoid visual clutter, I applied the lighting effect only to the outer outlines of characters."    
+  },
+  {
+    imgSrc: "pixel_outlinefinal.png", 
     title: "Applied to Selected Layers Only",
     text: "Despite being a fullscreen effect, the outlines are applied only to opaques in the Character layer, allowing characters to stand out in the visual hierarchy. I achieved the layer mask by writing a custom renderer feature in C#."            
-  },
-  {
-    imgSrc: "work_pixelart.png", 
-    title: "Colored by Lights",
-    text: "Outlines are realtime-lit in the main light's direction. To avoid visual clutter, I applied this only to the outer outlines of characters."    
-  },
-  {
-    imgSrc: "pixel_outlinegraph.png", 
-    title: "Full Shader Graph",
-    text: "The messy areas are from sampling the four neighboring pixels, which is essential for crisp 1-pixelwide outlines."    
-  },
-]
-
-const foliageDetails = [
-  {
-    imgSrc: "work_pixelart.png", 
-    title: "Spherized Custom Normals",
-    text: "For clean pixel art foliage, the leaf planes cannot cast shadows on each other, so I baked custom normals onto them as projected from a sphere mesh."            
-  },
-  {
-    imgSrc: "work_pixelart.png", 
-    title: "Perpendicular Fade",
-    text: "To further reduce noise, leaf planes fade out when their sides face the camera. Because their normals are already overwritten by the sphere, I reconstructed their normals in the shader using the cross product of partial derivatives (ddx and ddy)."    
-  },
-  {
-    imgSrc: "work_pixelart.png", 
-    title: "Foreground Darkening",
-    text: "Foliage is darkened near the camera, giving a nice definition to the foreground. To get a stepped color gradient, I smoothstepped and posterized the depth value."    
-  },
-  {
-    imgSrc: "pixel_foliagegraph.png", 
-    title: "Full Shader Graph",
-    text: "Includes wind animation in the vertex shader, and toon shading in the fragment shader."    
   },
 ]
 
 const miscDetails = [
+	{
+    imgSrc: "pixel_env_foliage.mp4", 
+    title: "Foreground Darkening",
+    text: "Foliage is darkened near the camera, giving a nice definition to the foreground. To get a stepped color gradient on the foliage, I smoothstepped and posterized the depth value."    
+  },
+  {
+    imgSrc: "pixel_vfx_bubbles.mp4", 
+    title: "Fake Metaballs",
+    text: "To mimic the goopy look of metaballs, I used a particle system of simple spheres -- squashed and stretched with animation curves -- thereby avoiding the performance cost of true metaball shaders."    
+  },
 	{
     imgSrc: "pixel_dither.mp4", 
     title: "Dithering",
     text: "Not used for the main shot above. I included this feature to support future characters with shaggy fur, sand, or other rough surfaces."            
   },
   {
-    imgSrc: "work_pixelart.png", 
+    imgSrc: "pixel_models.png", 
     title: "Models, Rigs and Animations",
     text: "I modeled, rigged and animated these two creatures in Maya! They were first designed for my pixel art Instagram."    
   },
@@ -82,7 +69,6 @@ export default function Pixelart()
               <p>All assets shown are mine -- shaders, character designs, models, rigs, animations, textures, VFX, UI, and font -- only the human uses a Mixamo rig and animation.</p>
             </div>
             <DetailsGroup groupName="Pixel Art Outline Shader" details={outlineDetails} />
-            <DetailsGroup groupName="Foliage Shader" details={foliageDetails} />
             <DetailsGroup groupName="Miscellaneous" details={miscDetails} />
           </div>
         </div>
