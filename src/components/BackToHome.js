00ea21eee514cfd7501ad5ed1403e35eb4840a37
Wayne Yip <wqyip97@gmail.com>
2024-08-07 20:54:23 +0800

    Center Back To Work button only on mobile

--- src/components/BackToHome.js
+++ src/components/BackToHome.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function BackToHome()
 {
 	return (
-		<div className="container max-w-screen-lg pb-10">
+		<div className="container max-w-screen-lg mx-auto px-2 pb-10">
 		<Link to="/">
-			<p className="text-blue-600 hover:text-blue-400">&lt; Back to Work</p>
+			<p className="md:text-left text-blue-600 hover:text-blue-400">&lt; Back to Work</p>
 		</Link>
 		</div>
 	)
